118c130b130c3c7b4c876d281c505a219d62d3d2
Eric Kiepushevski <e.b.kiepuszewski@student.tue.nl>
2023-12-10 00:19:08 +0100

    initial commit

---
+++ music.js
@@ -0,0 +1,21 @@
+
+var album_data = [
+    {
+        "name": "homeless n*gga pop music",
+        "artist": "Jim Legxacy",
+        "year": "2023",
+        "genre": ["indie", "pop"]
+    },
+    {
+        "name": "Se Bueno",
+        "artist": "TURQUOISEDEATH",
+        "year": "2023",
+        "genre": ["breakcore", "experimental"]
+    }
+]
+
+function displayAlbums() {
+    console.log(album_data);
+}
+
+displayAlbums();
